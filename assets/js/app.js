ad9ce1dc35ab20011c2a948f6cee436e06ffd904
JosuerBague <83200271+JosuerBague@users.noreply.github.com>
2021-10-13 19:03:42 +0800

    Working on percentage button.

--- assets/js/app.js
+++ assets/js/app.js
@@ -5,7 +5,8 @@ let txtHistory = '',
 let operable = {
   firstNum : '',
   operation : '',
-  secondNum : '', 
+  secondNum : '',
+  percentage : '', 
 }
 /* ########## END OF GLOBAL ########*/
 function addition (num1, num2) {
@@ -28,7 +29,7 @@ function operate(operator, num1, num2){
   if(operator === '+') return addition(num1, num2);
   if(operator === '-') return subtraction(num1, num2);
   if(operator === '*') return multiplication(num1, num2);
-  if(operator === '/') return division(num1,num2)
+  if(operator === '/') return division(num1, num2)
 }
 
 const displayHistory = document.querySelector('.display__history'),
@@ -37,6 +38,7 @@ const displayHistory = document.querySelector('.display__history'),
 const numbers = document.querySelectorAll('.number');
 numbers.forEach(btn => btn.addEventListener('click', function(e){
   
+  // prevents adding '0' padding 0 when there is no decimal place
   while (txtCurr[0] === '0' && txtCurr[1] !== '.') {
     txtCurr = txtCurr.substring(1)
   }
@@ -45,23 +47,30 @@ numbers.forEach(btn => btn.addEventListener('click', function(e){
   displayCurrent.textContent = txtCurr;
 }))
 
+// Clears everyhting from the operable object.
 const clear = document.querySelector('.opr-clear');
-clear.addEventListener('click', function(e){
+clear.addEventListener('click', function clear(){
   txtHistory = '';
   txtCurr = '0';
   displayHistory.textContent = txtHistory;
   displayCurrent.textContent = txtCurr;
+  txtCurr = '';
+  operable.firstNum = '';
+  operable.operation = '';
+  operable.secondNum = '';
 })
 
+// Clears the current entry a.k.a secondNum
 const clearEntry = document.querySelector('.opr-clear-entry');
-clearEntry.addEventListener('click', function(e){
+clearEntry.addEventListener('click', function clearEntry(){
   txtCurr = '0';
   displayCurrent.textContent = txtCurr;
+  operable.secondNum = '';
 })
 
 const num0 = document.querySelector('.num0');
 num0.addEventListener('click', function(e){
-  if(txtCurr.length > 1){
+  if(txtCurr.length >= 1){
     txtCurr += e.target.value;
     displayCurrent.textContent = txtCurr;
   }
@@ -81,36 +90,44 @@ mainOperations.forEach(operation => operation.addEventListener('click', function
 
   if (operable.firstNum === ''){
     operable.firstNum = txtCurr;
+    txtCurr = '';
   } else {
+
     operable.secondNum = txtCurr;
-    operable.firstNum = operate(operable.operation, parseInt(operable.firstNum), parseInt(operable.secondNum));
+    operable.firstNum = operate(operable.operation, parseFloat(operable.firstNum), parseFloat(operable.secondNum));
+    operable.secondNum = '';
+
   }
   txtHistory = `${operable.firstNum} ${operable.operation}`
-  txtCurr = '0';
+  txtCurr = '';
   displayHistory.textContent = txtHistory;
+  displayCurrent.textContent = `${operable.firstNum}`
 }));
 
-// const subBtn = document.querySelector('.opr-sub');
-// subBtn.addEventListener('click', function(e){
-//   txtHistory = txtCurr;
-//   txtCurr = '0';
-//   txtHistory += ` ${e.target.value}`;
-//   displayHistory.textContent = txtHistory;
-// })
 
-// const divideBtn = document.querySelector('.opr-divide');
-// divideBtn.addEventListener('click', function(e){
-//   txtHistory = txtCurr;
-//   txtCurr = '0';
-//   txtHistory += ` ${e.target.value}`;
-//   displayHistory.textContent = txtHistory;
-// })
+const percentage = document.querySelector('.opr-percent');
+percentage.addEventListener('click', function getPercentage(){
+  let percentOne = operable.firstNum ** 2 / 100,
+      percentTwo = operable.firstNum / 100;
 
-// const multiplyBtn = document.querySelector('.opr-multiply');
-// multiplyBtn.addEventListener('click', function(e){
-//   txtHistory = txtCurr;
-//   txtCurr = '0';
-//   txtHistory += ` ${e.target.value}`;
-//   displayHistory.textContent = txtHistory;
-// })
+  if (operable.firstNum === '' && operable.operation === ''){
+    txtCurr = '0';
+    displayCurrent.textContent = txtCurr;
+    displayHistory.textContent = txtCurr;
 
+  } else if (txtCurr === '' || txtCurr == percentOne || txtCurr == percentTwo) {
+    operable.percentage = txtCurr || parseInt(operable.firstNum)
+      
+    if(operable.operation === '+' || operable.operation === '-'){
+      txtCurr = (operable.percentage ** 2) / 100;
+      operable.percentage = txtCurr
+    } else {
+      txtCurr = operable.percentage / 100
+      operable.percentage = txtCurr
+    }
+
+    displayCurrent.textContent = txtCurr
+
+  }
+
+})
